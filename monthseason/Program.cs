d6f985693251adb1fe2a4004365dbc5b292d3e0a
yoojinhee <52348335+yhujinhui@users.noreply.github.com>
2020-05-06 15:59:17 +0900

    계절 출력하기

--- monthseason/Program.cs
+++ monthseason/Program.cs
@@ -10,6 +10,26 @@ namespace monthseason
     {
         static void Main(string[] args)
         {
+            Console.Write("월 입력: ");
+            String month = Console.ReadLine();
+
+            for(int i = 1; i <= 12; i++)
+            {
+                if (month.Contains(""+i))
+                {
+                    switch (i)
+                    {
+                        case 3: case 4:case 5:
+                            Console.Write("봄");break;
+                        case 6:case 7:case 8:
+                            Console.Write("여름"); break;
+                        case 10:case 11:case 9:
+                            Console.Write("가을"); break;
+                        default:
+                            Console.Write("겨울"); break;
+                    }
+                }
+            }
         }
     }
 }
